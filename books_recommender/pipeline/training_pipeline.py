bc916f401c5ab1b950ee402111f06b00a1d3c949
Sunil Bilgunde <sunilbilgunde1996@gmail.com>
2025-04-28 21:11:08 +0100

    model trainer added

--- books_recommender/pipeline/training_pipeline.py
+++ books_recommender/pipeline/training_pipeline.py
@@ -1,7 +1,7 @@
 from books_recommender.components.stage_00_data_ingestion import DataIngestion
 from books_recommender.components.stage_01_data_validation import Datavalidation
-
-
+from books_recommender.components.stage_02_data_transformation import DataTransformation
+from books_recommender.components.stage_03_model_trainer import ModelTrainer
 
 
 
@@ -10,6 +10,8 @@ class TrainingPipeline:
     def __init__(self):
         self.data_ingestion  =DataIngestion()
         self.data_validation = Datavalidation()
+        self.data_transformation = DataTransformation()
+        self.model_trainer = ModelTrainer()
 
     def start_training_pipeline(self):
         """
@@ -18,3 +20,5 @@ class TrainingPipeline:
         """
         self.data_ingestion.initiate_data_ingestion()
         self.data_validation.initiate_data_validation()
+        self.data_transformation.initiate_data_transformation()
+        self.model_trainer.initiate_model_trainer()
